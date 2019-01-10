1d48a9da6cc57e0aeb7badc2e81211082185ed23
Isaac Kwon <isaac-kwon@hotmail.com>
2019-01-10 09:52:44 -0800

    remove build folder from client/gitignore

---
+++ client/build/precache-manifest.748e790503ac26ef5d150b9e1fcf0f5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "91ba2d1ca70487cc26be",
+    "url": "/static/js/main.91ba2d1c.chunk.js"
+  },
+  {
+    "revision": "58cdc9341e11125eb936",
+    "url": "/static/js/1.58cdc934.chunk.js"
+  },
+  {
+    "revision": "91ba2d1ca70487cc26be",
+    "url": "/static/css/main.0cc78fc4.chunk.css"
+  },
+  {
+    "revision": "58cdc9341e11125eb936",
+    "url": "/static/css/1.2a1d400a.chunk.css"
+  },
+  {
+    "revision": "23412d5ec63615c6de5341200395a6ae",
+    "url": "/index.html"
+  }
+];
