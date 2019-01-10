783778c82547ee301a5d3bd86e3a9fabf28746bf
Isaac Kwon <isaac-kwon@hotmail.com>
2019-01-10 09:50:16 -0800

    refactor server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve('client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
